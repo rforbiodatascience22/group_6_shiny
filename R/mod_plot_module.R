3c13063564d2a8e893a5503efdba99ec4338322b
WilliamH-R <s194545@dtu.dk>
2022-03-28 09:25:20 +0000

    Small update

--- R/mod_plot_module.R
+++ R/mod_plot_module.R
@@ -4,28 +4,28 @@
 #'
 #' @param id,input,output,session Internal parameters for {shiny}.
 #'
-#' @noRd 
+#' @noRd
 #'
-#' @importFrom shiny NS tagList 
+#' @importFrom shiny NS tagList
 mod_plot_module_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+
   )
 }
-    
+
 #' plot_module Server Functions
 #'
-#' @noRd 
+#' @noRd
 mod_plot_module_server <- function(id){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
- 
+
   })
 }
-    
+
 ## To be copied in the UI
 # mod_plot_module_ui("plot_module_1")
-    
+
 ## To be copied in the server
 # mod_plot_module_server("plot_module_1")
