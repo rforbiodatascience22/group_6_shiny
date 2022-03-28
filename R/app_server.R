d4113b995a5b23c9b438eab9eaf2a1bc732a3360
urokoz <urokoz@gmail.com>
2022-03-28 09:03:44 +0000

    Added plot module

--- R/app_server.R
+++ R/app_server.R
@@ -3,7 +3,9 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom magrittr %>%
 #' @noRd
 app_server <- function(input, output, session) {
   # Your application server logic
+  mod_plot_module_server("plot_module_1")
 }
